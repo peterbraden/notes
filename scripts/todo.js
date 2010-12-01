763c879a36fbe8fd7e5928755b3a4cce649faae7
Peter Braden <PeterBraden@PeterBraden.co.uk>
2010-12-01 14:38:14 -0800

    Add tasks

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,5 +1,5 @@
 /*
-* 				todo.js
+*         todo.js
 * Notes todo app command line access
 
 usage:
@@ -18,15 +18,63 @@ $> todo do 183f
 
 */
 
-var sys = require('sys');
+var  sys = require('sys')
+  , request = require('request')
+  , scriptTools = require('scriptTools');
+
+var loadDB = function(opts, cb){
+  var h = {accept:'application/json', 'content-type':'application/json'}
+    , uri = "http://" + opts.user + ":" + opts.pwd + "@" + opts.host + "/" + opts.database;
+  request({uri: uri, headers:h}, function(err, response, body){
+    console.log(err,response, body);
+  });
+}
+
+
+var slugify = function(title){
+}
 
 
 
-var commands = {
-	
-	help : function(){
-		sys.print('See readme.txt');
-	}
+
+
+
+var commands = {  
+  help : function(){
+    console.log('See readme.txt');
+  },
+  
+  add : function(){
+    var args = Array.prototype.slice.call(arguments, 1)
+      , todo_obj = {
+          title : args.join(" "),
+          class : ['task'],
+          created : new Date().toISOString(),
+          modified : new Date().toISOString(),
+        };
+    
+    request({
+      method:'POST', 
+      body : JSON.stringify(todo_obj), 
+      uri : this.db_uri, 
+      headers : this.headers}, 
+      function(err, response, body){
+        if(err)
+          console.error(err);
+        
+        body = JSON.parse(body);
+          
+        if (body.ok){
+          console.log("Added:", body.id);
+          return;
+        }
+        
+        console.error(body);
+      });
+  }
+  
+  
+
 
 
 };
@@ -36,17 +84,29 @@ var commands = {
 
 
 
-
-
 if (process.argv.length<3){
-	sys.print('usage: todo command [args]');
+  sys.print('usage: todo command [args]');
+  
 } else{
-	if (commands[process.argv[2]]){
-		commands[process.argv[2]](process.argv);
-	} else {
-		sys.print('Unknown command');
-	}	
+  scriptTools.loadConfig(".notesconfig", function(conf){
+    /* Setup globals */
+    if (conf.user && conf.pwd && conf.host && conf.database)
+      conf.db_uri = "http://" + conf.user + ":" + conf.pwd + "@" + conf.host + "/" + conf.database;
+    else {
+      console.err("Config needs user, pwd, host and database")
+      return;
+    }
+    
+    conf.headers = {accept:'application/json', 'content-type':'application/json'}
+    
+    if (commands[process.argv[2]]){
+      var op = scriptTools.optParse(process.argv.slice(2));
+      conf['flags'] = op[0]
+      commands[process.argv[2]].apply(conf, op[1]);
+    } else {
+      console.log('Unknown command');
+    } 
+  }); 
 }
-sys.print('\n');
 
 
